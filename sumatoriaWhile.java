f309d4a2e906c83b1bbfefa81c7f98b3d4c12e07
dmacias1 <dmacias@bancodebogota.com.co>
2024-08-12 22:13:02 -0500

    Cargue sumatorias con FOR y While

---
+++ sumatoriaWhile.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+
+    {
+    int contador = 0;
+    int i = 1;
+
+        while (i <= 10) {
+
+                contador += i;
+                i++;
+            }
+        System.out.println("el resultado es: " +contador);
+    }
+}
